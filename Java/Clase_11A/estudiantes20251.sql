94c2e0919822dfb5699db7ca8851bb73a24d8891
Guardini Philama <Guardi2005@yahoo.fr>
2025-06-30 15:31:00 -0300

    Actualizar datos de estudiante en la tabla Estudiantes2025

--- Java/Clase_11A/estudiantes20251.sql
+++ Java/Clase_11A/estudiantes20251.sql
@@ -6,7 +6,7 @@ SELECT * FROM public."Estudiantes2025";
 INSERT INTO public."Estudiantes2025" (nombre, apellido, telefono, mail) VALUES ('Juan', 'Perez', '2614545456', 'juan@mail.com');
 
 -- Update (modificar)
-UPDATE public."Estudiantes2025" SET nombre='Juan Carlos', apellido='Garcia', mail='juajd@gmail.com' WHERE id_estudiantes2025= 1;
+UPDATE public."Estudiantes2025" SET nombre='calito ', apellido='fran', mail='jaime@gmail.com' WHERE id_estudiantes2025= 1;
 
 -- Delete(eliminar)
 DELETE FROM public."Estudiantes2025" WHERE id_estudiantes2025=3;
